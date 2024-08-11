c638103ea7214d857d040539918a1d1ce629c5a3
ThamilSelvan <tamilselvan1709@mail.com>
2024-08-11 21:28:51 +0530

    master update

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from master import Master,Slave
 IP_address = socket.gethostbyname(socket.gethostname())
 
 # Creating the Master and Slave objects
-
-
+slave = Slave("192.168.1.14",12345)
+slave.start()
 
--- master.py
+++ master.py
@@ -1,5 +1,7 @@
 import socket 
 import threading
+from pynput.keyboard import Listener,Key,Controller
+import json
 
 # This Main sys which sends the keyboard and mouse events to the slave
 class Master:
@@ -8,20 +10,34 @@ class Master:
         self.port = port
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clients = []   
-        
+    
+    def on_press(self,key,client):
+        print(key)
+        if key == Key.esc:
+            return False
+        data = json.dumps(str(key))
+        client.send(data.encode())
+       
     def start(self):
         self.s.bind((self.Ip_address, self.port))
         self.s.listen(1)
         print(f"[*] Listening as {self.Ip_address}:{self.port}")
         
         while True:
+            
             client, address = self.s.accept()
             print(f"[*] {address} connected")
             if client not in self.clients:
                 self.clients.append(client)
-               
+            
             print(f"[*] {address} connected")
             client.send("You are connected".encode())
+            with Listener(on_press=self.on_press) as listener:
+                listener.join()
+
+            
+            
+
             
     def __del__(self):
         self.s.close()
@@ -40,7 +56,10 @@ class Slave:
         
         while True:
             msg = self.s.recv(1024).decode()
+            data = json.load(msg)
+            print(data)
             print(f"[*] {msg}")
-            
+
+                
     def __del__(self):
         self.s.close()
