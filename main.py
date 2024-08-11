7e3472186909c1045fd91a8009ad4902aa88bd24
Thamilselvan <tamilselvan1709@gmail.com>
2024-08-11 21:58:04 +0530

    main file change

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from master import Master,Slave
 
 IP_address = socket.gethostbyname(socket.gethostname())
 
-# Creating the Master and Slave objects
-slave = Slave("192.168.1.14",12345)
-slave.start()
 
+
+if __name__ == "__main__":
+    options = input("Enter 1 for Master and 2 for Slave: ")
+    if options == "1":
+        master = Master(IP_address,12345)
+        master.start()
+    elif options == "2":
+        slave = Slave(" ",12345)    # IP address is not required for Slave              
+        slave.start()
+    else:
+        print("Invalid option")
